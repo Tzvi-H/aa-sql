eff72b34dd1bb6625d77228d5c18f498edcec079
Tzvi-H <16guitar@gmail.com>
2022-12-28 08:34:05 -0600

    Basic Phase 2 Create Tables Columns Constraints

--- practice-for-week-10-sql-1-long-practice-main/sql-practice/phase-2.sql
+++ practice-for-week-10-sql-1-long-practice-main/sql-practice/phase-2.sql
@@ -1,1 +1,17 @@
--- Your code here
+drop table if exists customers;
+drop table if exists coffee_orders;
+
+create table customers (
+  id  integer primary key,
+  name varchar(40) not null,
+  phone Numeric(10,0) unique,
+  email varchar(255) unique,
+  points integer default 5 not null,
+  created_at TIMESTAMP default CURRENT_TIME not null
+);
+
+create table coffee_orders (
+  id integer primary key,
+  is_redeemed boolean default false,
+  ordered_at TIMESTAMP default CURRENT_TIME not null
+);
